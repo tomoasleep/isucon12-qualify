cc60c45c74b682fa0429a5d5e8686f3f25e0f243
Tomoya Chiba <tomo.asleep@gmail.com>
2023-11-23 10:34:56 +0900

    Configure docker-compose to run ruby app

--- webapp/ruby/start-ruby.sh
+++ webapp/ruby/start-ruby.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -ex
 
+# if [ -d /home/isucon/tmp/ruby ]; then
+#   sudo chown -R $(id -u):$(id -g) /home/isucon/tmp/ruby
+# fi
+
 bundle install -j8
 exec bundle exec puma -e production -p ${SERVER_APP_PORT:-3000} -w 2
